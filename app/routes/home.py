b41f55a9492c2d6166f8a8b986ed8a5f728562d0
nishant-ketu <nishantketu51@gmail.com>
2019-08-20 18:34:56 +0530

    job status added

--- app/routes/home.py
+++ app/routes/home.py
@@ -31,7 +31,18 @@ class build(Resource):
         ('commands', api.payload['command']),
       )
       response = requests.post('http://127.0.0.1:8080/job/'+ api.payload['Job_name']+'/buildWithParameters', params=params, auth=('nishant', '11047f6c472c8d601da909a63377d31352'))
-      return response.status_code
+      time.sleep(30)
+      response = requests.get('http://127.0.0.1:8080/job/A_new_job/lastBuild/api/json', auth=('nishant', '11047f6c472c8d601da909a63377d31352'))
+      # print(response.text)
+      res = json.loads(response.text)
+      if res['building']:
+        time.sleep(30)
+      else:
+        if res['result'] == "SUCCESS":
+          return "Job was successfully build"
+        else:
+          return "Job has failed"
+
 
     @api.doc(parser=parser)
     def get(self):
@@ -43,53 +54,33 @@ class build(Resource):
         joblist.append(i["name"])
       return joblist
 
-    # @api.doc(parser=parser)
-    # def jenkins_job_status(self):
-    #   job_name = "A_job"
-    #   try:
-    #     url  = "https://127.0.0.1:8080/job/%s/lastBuild/api/json" %job_name   #Replace 'your_jenkins_endpoint' with your Jenkins URL
-    #     while True:
-    #       data = requests.get(url,auth=('nishant', '`11047f6c472c8d601da909a63377d31352`'))
-    #       if data['building']:
-    #         time.sleep(60)
-    #       else:
-    #         if data['result'] == "SUCCESS":
-    #           print("Job is success")
-    #           return True
-    #         else:
-    #           print("Job status failed")
-    #           return False
-    #   except Exception as e:
-    #     print(str(e))
-    #     return False
-
 @api.route('/create')
 class create(Resource):
+  @api.expect(Jenkins_configuration)
+  def post(self):
+    headers = {
+      'content-Type': 'text/xml',
+    }
+    params = (
+      ('name', 'A_new_job'),
+    )
+    data = open('/home/nishantketu/Documents/JenkinsAutomation/config.xml', 'rb').read()
     
-    @api.expect(Jenkins_configuration)
-    def post(self):
-      headers = {
-        'content-Type': 'text/xml',
-      }
-      params = (
-        ('name', 'A_new_job'),
-      )
-      data = open('/home/nishantketu/Documents/JenkinsAutomation/config.xml', 'rb').read()
-      
-      response = requests.post('http://127.0.0.1:8080/createItem', headers=headers, params=params, data=data, auth=('nishant', '11047f6c472c8d601da909a63377d31352'))
-      return response.status_code
+    response = requests.post('http://127.0.0.1:8080/createItem', headers=headers, params=params, data=data, auth=('nishant', '11047f6c472c8d601da909a63377d31352'))
+    return response.status_code
 
-# @api.route('/status')
-# class status(Resource):
-#   def get(self):
-#     job_name = "A_new_job"
-#     url  = "https://127.0.0.1:8080/job/%s/lastBuild/api/json" %job_name 
-#     data = requests.get(url,auth=('nishant', '11047f6c472c8d601da909a63377d31352'))
-#     if data['building']:
-#       time.sleep(60)
-#     else:
-#       if data['result'] == "SUCCESS":
-#         return "Job is success"
-#       else:
-#         return "Job status failed"
+@api.route('/status')
+class status(Resource):
+  @api.doc(parser=parser)
+  def get(self):
+    response = requests.get('http://127.0.0.1:8080/job/A_new_job/lastBuild/api/json', auth=('nishant', '11047f6c472c8d601da909a63377d31352'))
+    # print(response.text)
+    res = json.loads(response.text)
+    if res['building']:
+      time.sleep(60)
+    else:
+      if res['result'] == "SUCCESS":
+        return "Job was successfully build"
+      else:
+        return "Job has failed"
             
